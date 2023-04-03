7b377ed4fed8a607b27554efcfb0f081dc192fdd
PrasanthK97 <129174530+PrasanthK97@users.noreply.github.com>
2023-04-03 10:27:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+app.use(express.json());
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
@@ -33,6 +34,62 @@ const convertDbObjectToResponseObject = (dbObject) => {
     role: dbObject.role,
   };
 };
+
+app.get("/players/", async (request, response) => {
+  const getList = `
+    SELECT
+    *
+    FROM
+    cricket_team`;
+  const playerList = await db.all(getList);
+  response.send(
+    playerList.map((eachPlayer) => convertDbObjectToResponseObject(eachPlayer))
+  );
+});
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getList = `
+    SELECT
+    *
+    FROM
+    cricket_team
+    WHERE
+    player_id = ${playerId}`;
+  const player1 = await db.get(getList);
+  response.send(convertDbObjectToResponseObject(player1));
+});
+
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { player_name, jersey_number, role } = playerDetails;
+
+  const addPlayerDetails = `
+    INSERT INTO
+    cricket_team (player_name, jersey_number, role)
+    VALUES
+     (
+         ${player_name},
+         ${jersey_number},
+         ${role}
+      );`;
+
+  const dbResponse = db.run(addPlayerDetails);
+  const bookId = dbResponse.lastID;
+  response.send("Player Added to Team");
+});
+
+app.delete("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const deleteBookQuery = `
+    DELETE FROM
+      cricket_team
+    WHERE
+      player_id = ${playerId};`;
+  await db.run(deleteBookQuery);
+  response.send("Player Removed");
+});
+module.exports = app;
 
 app.get("/players/", async (request, response) => {
   const getList = `
